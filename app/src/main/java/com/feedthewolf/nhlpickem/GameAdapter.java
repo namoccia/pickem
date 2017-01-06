5e7eb01ccd72a782993fdc0d88fa70d43a15ce73
nmoccia <nicolas.a.moccia@gmail.com>
2017-01-06 14:38:16 -0700

    Support for team logos
    - Added all team logo SVG files
    - Updated list layout and GameAdapter to place the logo SVGs based on teamId

--- app/src/main/java/com/feedthewolf/nhlpickem/GameAdapter.java
+++ app/src/main/java/com/feedthewolf/nhlpickem/GameAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -51,11 +52,84 @@ public class GameAdapter extends BaseAdapter {
         // Get home element
         TextView homeTextView = (TextView) rowView.findViewById(R.id.game_list_home_text);
 
+        // Get image element
+        ImageView awayLogo = (ImageView) rowView.findViewById(R.id.game_list_away_image);
+        ImageView homeLogo = (ImageView) rowView.findViewById(R.id.game_list_home_image);
+
         Game game = (Game) getItem(position);
 
         awayTextView.setText(game.getAwayTeam().toString());
         homeTextView.setText(game.getHomeTeam().toString());
+        awayLogo.setImageResource(getImageResourceIdByTeamId(game.getAwayTeam().getId()));
+        homeLogo.setImageResource(getImageResourceIdByTeamId(game.getHomeTeam().getId()));
 
         return rowView;
+    }
+
+    public int getImageResourceIdByTeamId(int teamId) {
+        switch (teamId) {
+            case 21:
+                return R.drawable.ic_team_avalanche;
+            case 16:
+                return R.drawable.ic_team_blackhawks;
+            case 29:
+                return R.drawable.ic_team_blue_jackets;
+            case 19:
+                return R.drawable.ic_team_blues;
+            case 6:
+                return R.drawable.ic_team_bruins;
+            case 8:
+                return R.drawable.ic_team_canadiens;
+            case 23:
+                return R.drawable.ic_team_canucks;
+            case 15:
+                return R.drawable.ic_team_capitals;
+            case 53:
+                return R.drawable.ic_team_coyotes;
+            case 1:
+                return R.drawable.ic_team_devils;
+            case 24:
+                return R.drawable.ic_team_ducks;
+            case 20:
+                return R.drawable.ic_team_flames;
+            case 4:
+                return R.drawable.ic_team_flyers;
+            case 12:
+                return R.drawable.ic_team_hurricanes;
+            case 2:
+                return R.drawable.ic_team_islanders;
+            case 52:
+                return R.drawable.ic_team_jets;
+            case 26:
+                return R.drawable.ic_team_kings;
+            case 14:
+                return R.drawable.ic_team_lightning;
+            case 10:
+                return R.drawable.ic_team_maple_leafs;
+            case 22:
+                return R.drawable.ic_team_oilers;
+            case 13:
+                return R.drawable.ic_team_panthers;
+            case 5:
+                return R.drawable.ic_team_penguins;
+            case 18:
+                return R.drawable.ic_team_predators;
+            case 3:
+                return R.drawable.ic_team_rangers;
+            case 17:
+                return R.drawable.ic_team_red_wings;
+            case 7:
+                return R.drawable.ic_team_sabres;
+            case 9:
+                return R.drawable.ic_team_senators;
+            case 28:
+                return R.drawable.ic_team_sharks;
+            case 25:
+                return R.drawable.ic_team_stars;
+            case 30:
+                return R.drawable.ic_team_wild;
+            default:
+                return R.drawable.ic_team_wild;
+        }
     }
 }
